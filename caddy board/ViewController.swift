2be7e19b0329e4aee4eb17c5fc376c15c96f0a41
d-baez <63023353+d-baez@users.noreply.github.com>
2022-12-22 00:14:36 -0500

    created stroyboard and added sound file

--- caddy board/ViewController.swift
+++ caddy board/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    @IBAction func button1(_ sender: Any) {
+        let soundURL = Bundle.main.url(forResource: "caddysound1", withExtension: "m4a")
 
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+            audioPlayer?.play()
+        } catch {
+            print("Error playing sound")
+        }
+
+    }
+    
 
 }
 
