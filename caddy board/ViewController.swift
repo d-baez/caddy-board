da328ddd64bef5b1647945ea2932f95212a7c12d
d-baez <63023353+d-baez@users.noreply.github.com>
2022-12-22 23:59:47 -0500

    updated layout, top row works, updated constraint for futile size view

--- caddy board/ViewController.swift
+++ caddy board/ViewController.swift
@@ -18,8 +18,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
     }
-    @IBAction func button1(_ sender: Any) {
-        let soundURL = Bundle.main.url(forResource: "caddysound1", withExtension: "m4a")
+    @IBAction func button1(_ sender: UIButton)
+    // title: BADA BING BADA BOOM
+    {
+        // title: BADA BING BADA BOOM
+        sender.isSelected = true
+        let soundURL = Bundle.main.url(forResource: "caddy-sound1", withExtension: "m4a")
 
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
@@ -27,9 +31,86 @@ class ViewController: UIViewController {
         } catch {
             print("Error playing sound")
         }
+        DispatchQueue.main.asyncAfter(deadline: .now() + audioPlayer!.duration) {
+            // Set the button to the unselected state
+            sender.isSelected = false
+        }
+
+    }
+    @IBAction func button2(_ sender: UIButton)
+    // title : HEHEHE 911
+    {
+        sender.isSelected = true
+        let soundURL = Bundle.main.url(forResource: "caddysound2.1", withExtension: "mp3")
+
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+            audioPlayer?.play()
+        } catch {
+            print("Error playing sound")
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + audioPlayer!.duration) {
+            // Set the button to the unselected state
+            sender.isSelected = false
+        }
+
+    }
+    @IBAction func button3(_ sender: UIButton)
+    //title : THATS FIRE
+    {
+        sender.isSelected = true
+        let soundURL = Bundle.main.url(forResource: "caddy-sound3.1",withExtension: "m4a")
+
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+            audioPlayer?.play()
+        } catch {
+            print("Error playing sound")
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + audioPlayer!.duration) {
+            // Set the button to the unselected state
+            sender.isSelected = false
+        }
 
     }
     
+    @IBAction func button4(_ sender: UIButton)
+    //title :
+    {
+        sender.isSelected = true
+        let soundURL = Bundle.main.url(forResource: "soundname",withExtension: "mp3")
 
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+            audioPlayer?.play()
+        } catch {
+            print("Error playing sound")
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + audioPlayer!.duration) {
+            // Set the button to the unselected state
+            sender.isSelected = false
+        }
+        
+    }
+    
+    @IBAction func button5(_ sender: UIButton)
+    //title :
+    {
+        sender.isSelected = true
+        let soundURL = Bundle.main.url(forResource: "soundname",withExtension: "mp3")
+
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+            audioPlayer?.play()
+        } catch {
+            print("Error playing sound")
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + audioPlayer!.duration) {
+            // Set the button to the unselected state
+            sender.isSelected = false
+        }
+        
+    }
+    
 }
 
